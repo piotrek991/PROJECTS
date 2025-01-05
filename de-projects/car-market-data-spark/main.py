ec6f6df8b1c6d216138c84382461c28e0c6e053d
Piotr Sz <100309815+piotrek991@users.noreply.github.com>
2025-01-05 15:08:19 +0100

    added new field and render function

--- de-projects/car-market-data-spark/main.py
+++ de-projects/car-market-data-spark/main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from lxml import etree
 
 
-def get_data() -> BeautifulSoup:
+def get_data():
     ch_options = webdriver.ChromeOptions()
 
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
@@ -16,10 +16,9 @@ def get_data() -> BeautifulSoup:
 
     driver = webdriver.Chrome(options=ch_options)
     driver.get("https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc")
-    soup_inner = BeautifulSoup(driver.page_source, 'html.parser')
+    #soup_inner = BeautifulSoup(driver.page_source, 'html.parser')
 
-    driver.quit()
-    return soup_inner
+    return driver
 
 
 if __name__ == '__main__':
@@ -28,12 +27,14 @@ if __name__ == '__main__':
     dom = etree.HTML(str(site_data))
     search_data = dom.xpath("//div[@data-testid='search-results']/div/article")
 
-    for num, el in enumerate(dom.xpath("//div[@data-testid='search-results']/div/article")):
-        #advertisement_name = el.find_element(By.XPATH
-        #               , ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/a[@target='_self']")
+    for num, el in enumerate(site_data.find_elements(By.XPATH, "//div[@data-testid='search-results']/div/article")):
+        print(el.get_attribute('innerHTML'))
+        advertisement_name = el.find_element(By.XPATH
+                      , ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[2]/p")
+        print(advertisement_name.text)
         # print("soup search")
-        dom_inner = etree.HTML(str(el.text))
-        print(el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/a[@target='_self']")[0].text)
+        #dom_inner = etree.HTML(str(el.text))
+        #print(el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/a[@target='_self']")[0].text)
         # #print(soup.prettify())
         # print("selenium search")
         # print(advertisement_name.get_attribute('outerHTML'))
--- de-projects/car-market-data-spark/test.py
+++ de-projects/car-market-data-spark/test.py
@@ -5,6 +5,38 @@ import pandas as pd
 from pathlib import Path
 import os
 from operator import itemgetter
+import logging
+from datetime import datetime, timedelta
+from requests_html import HTMLSession
+#
+from pyppeteer import launch
+from requests_html import HTML
+import asyncio
+import traceback
+
+
+async def fetch(url, browser, ua):
+    page = await browser.newPage()
+    await page.setUserAgent(ua)
+
+    try:
+        await page.goto(url, {'waitUntil': 'load'})
+    except:
+        traceback.print_exc()
+    else:
+        return await page.content()
+    finally:
+        await page.close()
+
+
+async def main(page_url, page_ua):
+    browser = await launch(headless=True, args=['--no-sandbox'])
+
+    doc = await fetch(page_url, browser, page_ua)
+    await browser.close()
+
+    html = HTML(html=doc)
+    return html.html
 
 
 def encode_decode(str_data:str) -> str:
@@ -23,17 +55,36 @@ def save_get_data(data_dict: dict = None, org_df: pd.DataFrame = pd.DataFrame())
         return pd.DataFrame()
     else:
         pd_df = pd.DataFrame([[key, *list(values)] for key, values in data_dict.items()]
-                             , columns=['el_id', 'engine_capacity', 'engine_power', 'ds1_rest', 'price_str'])
+                             , columns=['el_id', 'notice_name','engine_capacity', 'engine_power', 'price_str',
+                                        'mileage', 'fuel_type', 'gearbox', 'production_year', 'when_added', 'ds1_rest']
+                             )
         pd_df = pd.concat([pd_df.reset_index(drop=True), org_df.reset_index(drop=True)], axis=0)
         print(pd_df)
         pd_df.to_csv(data_path, index=False)
         return
 
 
+def resolve_time(start_time: datetime, data_string:str) -> None | datetime:
+    data_string = data_string.replace("wczoraj", "1 dni temu")
+    if str_val := re.search('([0-9]+)\\s+(minut[ęy]?)', data_string):
+        f_time = start_time - timedelta(minutes=int(str_val.group(1)))
+    elif str_val:= re.search('([0-9]+)\\s+(godzin[ęy]?)', data_string):
+        f_time = start_time - timedelta(hours=int(str_val.group(1)))
+    elif str_val := re.search('([0-9]+)\\s+(dni)', data_string):
+        f_time = start_time - timedelta(days=int(str_val.group(1)))
+    else:
+        print(f"currently {data_string} could not be resolved")
+        return
+    return f_time
+
+
 if __name__ == "__main__":
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'}
-    data = requests.get("https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc&page=3", headers=headers)
-    dom = etree.HTML(str(data.content))
+    URL = 'https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc&page=3'
+    UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    data = asyncio.run(main(URL, UA))
+
+    execution_start = datetime.now()
+    dom = etree.HTML(str(data))
 
     data_dict_o = dict()
     data_current_pd = save_get_data()
@@ -46,6 +97,7 @@ if __name__ == "__main__":
     for num, el in enumerate(dom.xpath("//div[@data-testid='search-results']/div/article")):
         dom_inner = etree.HTML(str(el.text))
         el_id = el.get('data-id')
+        ###dimensions###
         notice_name = encode_decode(
             el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')][2]/*/a[@target='_self']")[0] \
             .text)
@@ -55,24 +107,46 @@ if __name__ == "__main__":
         price_str = int(encode_decode(
             el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/h3")[0].text.replace(" ", "")
         ))
+        mile_age = int(re.sub(r'\s|km','',encode_decode(
+            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='mileage']/text()")[0]
+            )
+        ))
+        fuel_type = encode_decode(
+            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='fuel_type']/text()")[0]
+        )
+        gearbox = encode_decode(
+            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='gearbox']/text()")[0]
+        )
+        production_year = int(encode_decode(
+            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='year']/text()")[0]
+        ))
+        when_added = resolve_time(execution_start, encode_decode(
+            el.xpath(
+                ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[2]/p")[0]
+        )).strftime("%d-%m-%Y %H:%M:%S")
         engine_capacity = re.match("(^[0-9\\s]+cm3\\s+)•", describe_str_1).group(1) \
                 if re.match("(^[0-9\\s]+cm3\\s+)•", describe_str_1) else "NOT SPECIFIED"
         engine_power = re.search("•([0-9\\s]*KM\\s+)•", describe_str_1).group(1) \
                 if re.search("•([0-9\\s]*KM\\s+)•", describe_str_1) else "NOT SPECIFIED"
         ds1_rest = re.search("(•\\s+)([^•]+$)", describe_str_1).group(2)
+        ###dimensions###
 
         data_dict_o[int(el_id)] = [
-            engine_capacity
+            notice_name
+            , engine_capacity
             , engine_power
-            , ds1_rest
             , price_str
+            , mile_age
+            , fuel_type
+            , gearbox
+            , production_year
+            , when_added
+            , ds1_rest
         ]
     diff_el_id = list(set(data_dict_o.keys()).difference(set(current_ids)))
     if diff_el_id:
         data_list_o = list(itemgetter(*diff_el_id)(data_dict_o))
         data_dict_o = {val: data_list_o[num] for num, val in enumerate(diff_el_id)}
-        print(f"before save")
-        print(data_dict_o)
         save_get_data(data_dict_o, data_current_pd)
     else:
         print("nothing to save")
@@ -80,7 +154,5 @@ if __name__ == "__main__":
 
 
 
-####match cases
-# pojemność silnik (^[0-9\s]+cm3\s+)(?:•
-# moc silnika (?:•)([0-9\s]*KM\s+)(?:•)
-# reszta (?:•)([^•]+$)
+### match case
+## minut ([0-9]+)\s+(minut[ęy]?)
