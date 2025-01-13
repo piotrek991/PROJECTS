f640979b7fad8197041bb371771a19427cab1822
Piotr Sz <100309815+piotrek991@users.noreply.github.com>
2025-01-13 23:06:39 +0100

    added diff and extract functions for otomoto object

--- de-projects/car-market-data-spark/test.py
+++ de-projects/car-market-data-spark/test.py
@@ -43,15 +43,15 @@ class HtmlContent:
 
         return url_components._replace(query=updated_query).geturl()
 
-    @staticmethod
-    def edit_url_param(url: str, param_name: str, param_new_val: any):
+    def edit_url_param(self, url: str, param_name: str, param_new_val: any):
         url_components = urlparse(url)
         params = parse_qs(url_components.query)
         try:
             assert params.get(param_name)
         except AssertionError as e:
-            print(f"url does not contain specified param - please add param before trying to modify it.")
-            raise e
+            url_inner = self.add_url_params(self.main_url, {param_name:param_new_val})
+            print(f"url does not contain specified param - ADDING.")
+            return url_inner
         params[param_name] = param_new_val
         updated_query = urlencode(params, doseq=True)
         return url_components._replace(query=updated_query).geturl()
@@ -139,7 +139,8 @@ class OtoMotoData(HtmlContent):
 
     def get_stored_data(self, alt_path: str = None):
         try:
-            inner_path = self.data_path if not alt_path else os.path.abspath(alt_path)
+            inner_path = os.path.join(self.data_path, self.def_file_name) if not alt_path \
+                else os.path.join(os.path.abspath(alt_path), self.def_file_name)
             assert os.path.isfile(inner_path)
         except AssertionError as e:
             print(f"Provided path is not file.")
@@ -162,87 +163,89 @@ class OtoMotoData(HtmlContent):
         pd_inner = pd.concat([pd_inner.reset_index(drop=True), self.stored_data.reset_index(drop=True)], axis=0)
         pd_inner.to_csv(os.path.join(inner_path, self.def_file_name), index=False)
 
-    def diff_data(self): #check if current ids are occupied
-        pass
-    def extract_fields(self): #extract data from html
-        pass
+    def diff_data(self, key_field: str):
+        try:
+            assert self.stored_data
+            inner_ids = pd.Series(self.stored_data[key_field]).to_list()
+            diff_ids = list(set(self.new_data.keys()).difference(set(inner_ids)))
+        except AssertionError as e:
+            print(f"Empty stored data - returning object new data.")
+            return self.new_data
+        except Exception as e:
+            print(f"Error occurred during checking diff")
+            raise e
+        inner_diff_items = list(itemgetter(*diff_ids)(self.new_data))
+        inner_diff_dict = {inner_val: inner_diff_items[inner_num] for inner_num, inner_val in enumerate(diff_ids)}
+        return inner_diff_dict
 
+    def extract_fields(self):
+        self.html_data = asyncio.run(self.main(use_temp=False))
+        html_etree = etree.HTML(str(self.html_data))
+
+        for num, el in enumerate(html_etree.xpath("//div[@data-testid='search-results']/div/article")):
+            el_id = el.get('data-id')
+            ###dimensions###
+            notice_name = self.self.encode_decode(
+                el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')][2]/*/a[@target='_self']")[0] \
+                    .text)
+            describe_str_1 = self.encode_decode(
+                el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')][2]/p")[0].text
+            )
+            price_str = int(self.encode_decode(
+                el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/h3")[0].text.replace(" ", "")
+            ))
+            mile_age = int(re.sub(r'\s|km', '', self.encode_decode(
+                el.xpath(
+                    ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='mileage']/text()")[
+                    0]
+            )
+                                  ))
+            fuel_type = self.encode_decode(
+                el.xpath(
+                    ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='fuel_type']/text()")[
+                    0]
+            )
+            gearbox = self.encode_decode(
+                el.xpath(
+                    ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='gearbox']/text()")[
+                    0]
+            )
+            production_year = int(self.encode_decode(
+                el.xpath(
+                    ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='year']/text()")[
+                    0]
+            ))
+            when_added = self.resolve_time(execution_start, self.encode_decode(
+                el.xpath(
+                    ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[2]/p")[0].text
+            )).strftime("%d-%m-%Y %H:%M:%S")
+            engine_capacity = re.match("(^[0-9\\s]+cm3\\s+)•", describe_str_1).group(1) \
+                if re.match("(^[0-9\\s]+cm3\\s+)•", describe_str_1) else "NOT SPECIFIED"
+            engine_power = re.search("•([0-9\\s]*KM\\s+)•", describe_str_1).group(1) \
+                if re.search("•([0-9\\s]*KM\\s+)•", describe_str_1) else "NOT SPECIFIED"
+            ds1_rest = re.search("(•\\s+)([^•]+$)", describe_str_1).group(2)
+
+            inner_dict = {
+                'notice_name': notice_name
+                , 'engine_capacity': engine_capacity
+                , 'engine_power': engine_power
+                , 'price_str': price_str
+                , 'mile_age': mile_age
+                , 'fuel_type': fuel_type
+                , 'gearbox': gearbox
+                , 'production_year': production_year
+                , 'when_added': when_added
+                , 'ds1_rest': ds1_rest
+            }
+            self.new_data[int(el_id)] = itemgetter(*list(self.fields))(inner_dict)
+
+    def next_page(self):
+        pass
 if __name__ == "__main__":
     execution_start = datetime.now()
     URL = 'https://www.otomoto.pl/osobowe?search%5Border%5D=created_at_first%3Adesc&page=3'
     UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    data = asyncio.run(main(URL, UA))
-    print(f"html content retrieved after {datetime.now() - execution_start}")
-    t1 = datetime.now()
-    dom = etree.HTML(str(data))
 
-    data_dict_o = dict()
-    data_current_pd = save_get_data()
-    try:
-        assert not data_current_pd.empty
-        current_ids = pd.Series(data_current_pd.el_id).to_list()
-    except AssertionError:
-        current_ids = []
-
-    for num, el in enumerate(dom.xpath("//div[@data-testid='search-results']/div/article")):
-        dom_inner = etree.HTML(str(el.text))
-        el_id = el.get('data-id')
-        ###dimensions###
-        notice_name = encode_decode(
-            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')][2]/*/a[@target='_self']")[0] \
-            .text)
-        describe_str_1 = encode_decode(
-            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')][2]/p")[0].text
-        )
-        price_str = int(encode_decode(
-            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/h3")[0].text.replace(" ", "")
-        ))
-        mile_age = int(re.sub(r'\s|km','',encode_decode(
-            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='mileage']/text()")[0]
-            )
-        ))
-        fuel_type = encode_decode(
-            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='fuel_type']/text()")[0]
-        )
-        gearbox = encode_decode(
-            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='gearbox']/text()")[0]
-        )
-        production_year = int(encode_decode(
-            el.xpath(".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[@data-parameter='year']/text()")[0]
-        ))
-        when_added = resolve_time(execution_start, encode_decode(
-            el.xpath(
-                ".//div[not(@id='financing-widget-listing-card-entrypoint')]/*/dd[2]/p")[0].text
-        )).strftime("%d-%m-%Y %H:%M:%S")
-        engine_capacity = re.match("(^[0-9\\s]+cm3\\s+)•", describe_str_1).group(1) \
-                if re.match("(^[0-9\\s]+cm3\\s+)•", describe_str_1) else "NOT SPECIFIED"
-        engine_power = re.search("•([0-9\\s]*KM\\s+)•", describe_str_1).group(1) \
-                if re.search("•([0-9\\s]*KM\\s+)•", describe_str_1) else "NOT SPECIFIED"
-        ds1_rest = re.search("(•\\s+)([^•]+$)", describe_str_1).group(2)
-        ###dimensions###
-
-        data_dict_o[int(el_id)] = [
-            notice_name
-            , engine_capacity
-            , engine_power
-            , price_str
-            , mile_age
-            , fuel_type
-            , gearbox
-            , production_year
-            , when_added
-            , ds1_rest
-        ]
-    print(f"dimensions processed after {datetime.now() - t1}")
-    t1 = datetime.now()
-    diff_el_id = list(set(data_dict_o.keys()).difference(set(current_ids)))
-    if diff_el_id:
-        data_list_o = list(itemgetter(*diff_el_id)(data_dict_o))
-        data_dict_o = {val: data_list_o[num] for num, val in enumerate(diff_el_id)}
-        save_get_data(data_dict_o, data_current_pd)
-    else:
-        print("nothing to save")
-    print(f"data saved after {datetime.now() - t1}")
 
 
 
